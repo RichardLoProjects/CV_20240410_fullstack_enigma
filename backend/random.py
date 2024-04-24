36740aa05d52d3e0d52ecb2cd8ac161570b4d165
RichardLoProjects <richardlo.business@gmail.com>
2024-04-24 09:18:15 +0100

    add_config_randomiser

---
+++ backend/random.py
@@ -0,0 +1,15 @@
+import random
+from enigma_machine import *
+
+def randomise_enigma() -> EnigmaMachine:
+    pass
+
+
+
+
+def main():
+    ## Testing code would go here
+    pass
+
+if __name__ == '__main__':
+    main()
